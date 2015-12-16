6bed662cfcf15fe5ddd7fee8a395f55c1ec05f51
wajahat-javaid <vajahat.javaid@gmail.com>
2015-12-16 14:03:15 +0100

    First Commit

---
+++ src/main/java/com/sonarqube/example/SonarqubeTestApplication.java
@@ -0,0 +1,12 @@
+package com.sonarqube.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarqubeTestApplication.class, args);
+    }
+}
